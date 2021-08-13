438b021049f598dd96f2052b7c5d551b523c237c
Andrea Rodrigues <a.r.andre@outlook.com>
2021-08-13 15:13:23 -0300

    Completei o script incluirPet ( consultarPet (Get)

---
+++ src/test/java/petstore/Pet.java
@@ -0,0 +1,78 @@
+// 1 - Pacote
+package petstore;
+
+// 2 - Bibliotecas
+
+import io.restassured.response.ValidatableResponse;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static io.restassured.RestAssured.basePath;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.Matchers.contains;
+
+// 3 - Classe
+public class Pet {
+    //3.1 - Atributos
+    String uri = "https://petstore.swagger.io/v2/pet"; // endereço da entidade Pet
+
+    //3.2 - Métodos e Funções
+    public String lerJson(String caminhoJson) throws IOException {
+        return new String(Files.readAllBytes(Paths.get(caminhoJson)));
+    }
+
+    //Incluir - Create - Post
+    @Test(priority = 1) // Identifica o método ou função como um teste para o TestNG
+    public void incluirPet() throws Exception {
+        String jsonBody = lerJson ("db/pet1.json");
+
+        // Sintaxe Gherkin
+        // Dado - Quando - Então
+        // Given - When - Then
+
+        given() // Dado
+                .contentType("application/json") // comum em API REST - antigos eram "text/xml"
+                .log().all()
+                .body(jsonBody)
+        .when() // Quando
+                .post(uri)
+        .then() // Então
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Apolo"))
+                .body("status", is("available"))
+                .body("category.name", is("AX2345LORT"))
+                .body("tags.name", contains("data"))
+        ;
+
+
+    }
+
+    @Test(priority = 2)
+    public void consultarPet(){
+        String petId = "1809199718";
+
+        String token =
+        given()
+                .contentType("application/json")
+                .log().all()
+        .when()
+                .get(uri + "/" + petId)
+        .then()
+                .log().all()
+                .statusCode(200)
+                .body("name", is("Apolo"))
+                .body("category.name", is("AX2345LORT"))
+                .body("status", is("available"))
+        .extract()
+                .path("category.name")
+
+        ;
+        System.out.println("O token é " + token);
+
+    }
+}
